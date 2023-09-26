5594b43a4019a2b9c71655f52b083180efa6a6a4
Harsh Patel <harsh3608@gmail.com>
2023-09-26 18:48:27 +0530

    ...

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './ExpenseItem.css'
 
-function ExpenseItem() {
+function ExpenseItem(props) {
     const expenseDate = new Date(2023,9,8);
     const expenseTitle = 'Car Insurance';
     const expenseAmount = 500.00;
@@ -9,7 +9,7 @@ function ExpenseItem() {
 
     return (
         <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
                 <h2>{expenseTitle}</h2>
                 <div className="expense-item__price">${expenseAmount}</div>

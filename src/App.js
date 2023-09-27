424a35bf77fe27734cf9e4f849e64b1ce0ac1a5c
Harsh Patel <harsh3608@gmail.com>
2023-09-27 15:01:22 +0530

    worked on adding new expense

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/Expenses/NewExpense";
 
 const App = () => {
   return (
     <div>
+      <NewExpense/>
       <Expenses/>
     </div>
   );

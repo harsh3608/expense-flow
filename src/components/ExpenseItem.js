9c0f9339be8c967a17b44f310661c261ea69d4e2
Harsh Patel <harsh3608@gmail.com>
2023-09-27 09:56:15 +0530

    rectified error in expense item

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,17 +2,14 @@ import React from "react";
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
-    const expenseDate = new Date(2023,9,8);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 500.00;
 
 
     return (
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
+            <div>{props.date}</div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     );
